45440b22e25658d7c421568fea74e648832827ec
ferifajri <55946443+ferifajri@users.noreply.github.com>
2019-11-23 15:03:22 +0700

    Update BigQuery.py

--- credentials/BigQuery.py
+++ credentials/BigQuery.py
@@ -1,8 +1,8 @@
 def BQ_json():
     # Put Your Google JSON credential file path here ex:  "/home/work/myjsonfile.json"  "
-    json_path = "C:\Code\Datadog\\venv\\billing-production-infra-29ba-02abdd9608dc.json"
+    json_path = "/home/work/myjsonfile.json"
     # put your service_account here : myserviceaccountt@domain.iam.gserviceaccount.com
-    service_account = "csv-import@billing-production-infra-29ba.iam.gserviceaccount.com"
+    service_account = "myserviceaccountt@domain.iam.gserviceaccount.com"
     # put your Project_ID here
-    project_id = "billing-production-infra-29ba"
+    project_id = "myProject_ID"
     return json_path, service_account, project_id
